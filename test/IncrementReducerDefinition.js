dbf8d93371b8a03f93cac5b5f96bf6c16f044262
Josh Perez <josh@goatslacker.com>
2016-03-22 16:42:34 -0700

    Docs

--- test/IncrementReducerDefinition.js
+++ test/IncrementReducerDefinition.js
@@ -1,12 +1,23 @@
+// Contains a set of action creators
 import counter from './CounterActionTypes'
 
+// The initial state the reducer will have
+// similar to `this.state = 0` in Alt
 export const state = 0
 
+// Which actions the reducer handles.
+// The key maps to the function declared in this file
+// The value is an Array of actions that should be handled by this function
+// similar to `this.bindListeners` in Alt
 export const mapActionTypes = {
   incremented: [counter.incremented],
   twentyFive: [counter.twentyfive],
 }
 
+// An individual reducer of an action
+// It gets the reducer's entire state and returns the new state of the reducer
+// this particular reducer is only called whenever the counter.incremented
+// action is dispatched
 export const incremented = (state, payload) => {
   return state + payload
 }
